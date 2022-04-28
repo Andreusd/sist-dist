ed700435f26695596817229645296f3d4fa7b041
Andreusd <andreuzielsd@gmail.com>
2022-04-27 23:40:46 -0300

    fix: bugs on Lab2

--- Lab2/funcoes.py
+++ Lab2/funcoes.py
@@ -1,5 +1,11 @@
+import re
+
+
 def palavras_mais_frequentes(texto):
     contagem_palavras = {}
-    for palavra in texto.replace(',', ' ').replace('.', ' ').replace('\n', ' ').split(' '):
+    # trata o conteudo do arquivo, removendo espacos e quebra de linha
+    for palavra in re.sub('( +)', ' ', texto.replace('\n', ' ').replace('.', ' ').replace(',', ' ')).split(' '):
+        # conta as ocorrencias de uma palavra
         contagem_palavras[palavra] = contagem_palavras.get(palavra, 0) + 1
+    # ordena a lista de acordo com o numero de ocorrencias da palavra e junta essa lista em uma string
     return ' '.join(sorted(contagem_palavras.keys(), key=contagem_palavras.get, reverse=True)[:5])
--- Lab2/servidor.py
+++ Lab2/servidor.py
@@ -1,7 +1,6 @@
 # Aluno Andre Uziel 119051475 2022.1
 
 import socket
-import re
 
 from funcoes import palavras_mais_frequentes
 
@@ -15,20 +14,20 @@ sock.bind((HOST, PORT))
 sock.listen(1)
 
 while True:
-
     newSock, addr = sock.accept()
     print("Conectado com:" + str(addr))
-
-    while True:
-        msg = newSock.recv(1024)
-        if not msg:
-            break
+    # cliente informa o nome do arquivo
+    while msg := newSock.recv(1024):  # operador := disponivel no python 3.8
+        # digitar uma mensagem vazia encerra a conexao com o cliente
         try:
+            # tenta abrir o arquivo com o nome que o cliente forneceu
             with open(file=str(msg, encoding='utf-8'), mode='r', encoding='utf-8') as arquivo:
                 texto = arquivo.read()
+            # envia o conteudo do arquivo tratado para o cliente
             newSock.send(palavras_mais_frequentes(texto).encode())
+        # captura a excessao quando nao acha o arquivo
         except FileNotFoundError:
             newSock.send(b"Erro: o arquivo nao foi encontrado")
 
     newSock.close()
-    sock.close()
+sock.close()
