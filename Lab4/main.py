8f80f96624b9d1d5253458e1cc3ee3e42576008e
Andreusd <andreuzielsd@gmail.com>
2022-06-26 20:12:13 -0300

    feat: permite o uso de hosts diferentes

--- Lab4/main.py
+++ Lab4/main.py
@@ -12,16 +12,23 @@ processos = []  # lista que armazena os processos que estão executando
 
 def carrega_configuracao() -> dict:  # carrega o arquivo de configuração
     with open("configuracao.json", "r") as arquivo:
-        return json.load(arquivo)
+        configuracao: dict = json.load(arquivo)
+    for node in configuracao.values():
+        # transforma a configuração de forma a deixar exposto o host e porta de cada node vizinho
+        node["vizinhos"] = list(map(lambda vizinho: (
+            configuracao[vizinho]["host"], vizinho), node["vizinhos"]))
+    return configuracao
 
 
 def incia_node(porta: str, dados: dict):  # inicia os nós
     # escolhe o valor armazenado por cada nó (valor comparado)
     valor = random.randrange(MAX_INDEX)
+    vizinhos_formatado = ", ".join(
+        map(lambda viz: f"{viz[1]}({viz[0]})", dados['vizinhos']))
     print(
-        f"-> Iniciando node {porta} com vizinhos {dados['vizinhos']} no {dados['servidor']} com valor {valor}")
+        f"-> Iniciando node {porta}({dados['host']}) com vizinhos {vizinhos_formatado} com valor {valor}")
     processo = multiprocessing.Process(target=novo_processo, args=(
-        dados['servidor'], porta, valor, dados['vizinhos']))
+        dados['host'], porta, valor, dados['vizinhos']))
     processo.start()
     processos.append(processo)
 
@@ -30,13 +37,13 @@ def main():
     nodes = carrega_configuracao()
     for indice, dados in nodes.items():
         incia_node(indice, dados)
-    print("entre com o identificador do no que vai iniciar a eleicao")
+    print("Entre com a porta do node que vai iniciar a eleicao (ex: 5000)")
     entrada = None
     while True:
         entrada = input()
-        if entrada in nodes.keys():
+        if entrada in nodes:
             # conecta com o nó que será a raiz para que ele inicie a eleição
-            conn = rpyc.connect('localhost', entrada)
+            conn = rpyc.connect(nodes[entrada]['host'], entrada)
             conn.root.probe(1)  # probe(1) significa que o no é raiz
             break
         else:
--- Lab4/node.py
+++ Lab4/node.py
@@ -2,7 +2,7 @@ import rpyc
 from rpyc.utils.server import ThreadedServer
 
 
-def novo_processo(servidor: str, porta: int, indice: int, vizinhos: list):
+def novo_processo(host: str, porta: int, indice: int, vizinhos: list):
     pai = None  # o no raiz sempre tera pai '1'
     max_valor = 0  # armazena o maior valor entre os filhos
     porta_maior = None  # armazena a porta do nó com maior valor
@@ -14,18 +14,20 @@ def novo_processo(servidor: str, porta: int, indice: int, vizinhos: list):
         def exposed_probe(self, origem):
             nonlocal pai  # referencia a variavel da funcao novo_processo
             if(pai):  # caso esse nó já tenha sido visitado anteriormente
-                conn = rpyc.connect('localhost', origem)
+                # origem é uma tupla em que origem[0] é o host e origem[1] a porta
+                conn = rpyc.connect(origem[0], origem[1])
                 # enviamos ack para informar que ja foi visitado
                 conn.root.ack(porta)
                 return
             pai = origem  # define o pai desse nó
             for vizinho in vizinhos:
                 if vizinho != pai:  # envia probe para todos os vizinhos exceto o pai
-                    print(f"-> {porta}: visita {vizinho}")
-                    conn = rpyc.connect('localhost', vizinho)
-                    conn.root.probe(porta)  # envia probe para os vizinhos
+                    print(f"-> {porta}: visita {vizinho[1]}")
+                    conn = rpyc.connect(vizinho[0], vizinho[1])
+                    # envia probe para os vizinhos
+                    conn.root.probe((host, porta))
             if(origem != 1):  # se o nó não for a raiz, envia echo para o pai
-                conn = rpyc.connect('localhost', pai)
+                conn = rpyc.connect(pai[0], pai[1])
                 maior_valor = max(max_valor, indice)
                 # envia echo para o pai com o maior valor entre o proprio indice e o dos filhos
                 conn.root.echo(porta, maior_valor,
@@ -34,7 +36,7 @@ def novo_processo(servidor: str, porta: int, indice: int, vizinhos: list):
                 # se o nó for raiz, notifica todos os nos do novo lider
                 maior_valor = max(max_valor, indice)
                 for vizinho in vizinhos:
-                    conn = rpyc.connect('localhost', vizinho)
+                    conn = rpyc.connect(vizinho[0], vizinho[1])
                     conn.root.lider(
                         maior_valor, porta_maior if maior_valor == max_valor else porta)
 
@@ -57,8 +59,8 @@ def novo_processo(servidor: str, porta: int, indice: int, vizinhos: list):
                 f"-> {porta}: reconhece {lider_eleito} como lider com valor {lider_valor}")
             for vizinho in vizinhos:
                 if vizinho != pai:
-                    conn = rpyc.connect('localhost', vizinho)
+                    conn = rpyc.connect(vizinho[0], vizinho[1])
                     conn.root.lider(lider_valor, lider_porta)
 
-    server = ThreadedServer(Node, hostname=servidor, port=porta)
+    server = ThreadedServer(Node, hostname=host, port=porta)
     server.start()
